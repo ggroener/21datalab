3c5d1d8866670bb487b2f81a40359bd9a198d2aa
smartyal <albert_krohn@yahoo.de>
2021-03-05 12:04:21 +0100

    some improvements for the installation and trial: - occupancydemo.bat executable from any folder - requirements updated - removed private plugin folder from demo

--- scripts/occupancydemo.bat
+++ scripts/occupancydemo.bat
@@ -3,9 +3,12 @@ set "HTTP_PROYX="
 set "HTTPS_PROXY="
 set "http_proxy="
 set "https_proxy="
-set mypath=%cd%
+
+set mydrive=%~d0
+%mydrive%
+set mypath=%~dp0
 cd %mypath%\..
-start python restservice.py occupancydemo --plugin_directory private
+start python restservice.py occupancydemo
 start bokeh serve bokeh_web --allow-websocket-origin="*" --port 5006 --args http://127.0.0.1:6001/ root.visualization.workbench
 start bokeh serve bokeh_web --allow-websocket-origin="*" --port 5010 --args http://127.0.0.1:6001/ root.visualization.selfservice0
 endlocal
